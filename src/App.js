1874db9f84384b8919cc0d65679b4931fd80e7fa
Chloe Lee <chloelee2311@gmail.com>
2024-04-23 22:55:35 +1200

    init cra

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello</h1>
+        <p1>sleep</p1>
+        <button>hi</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
