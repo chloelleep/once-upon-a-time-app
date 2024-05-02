f5ebe13c1f0ac95a8176a14f24fedb170fd76bb5
Chloe Lee <chloelee2311@gmail.com>
2024-05-02 13:43:51 +1200

    delete react logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>hello</h1>
         <p1>sleep</p1>
         <button>hi</button>
