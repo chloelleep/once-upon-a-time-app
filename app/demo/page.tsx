d40a5883b4f6a92ea65888f56404878401e0eb0b
zerosicx <hcsofteng@gmail.com>
2024-05-22 11:55:46 +1200

    Demo page that changs dynamically

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import NavBar from './_components/NavBar'
+import DisplayStory from './_components/DisplayStory';
 
 type Props = {}
 
 const DemoPage = (props: Props) => {
     const dummyData = Array(5).fill(0);
+    const dummyStoryFiles = [
+        {
+            name: "demo story",
+            filesource: "/demo.html"
+        },
+        {
+            name: "demo story",
+            filesource: "/demo.html"
+        },
+        {
+            name: "demo story",
+            filesource: "/demo.html"
+        },
+        {
+            name: "demo story",
+            filesource: "/demo.html"
+        }
+    ]
+
   return (
     <div>
         <NavBar></NavBar>
@@ -15,9 +35,12 @@ const DemoPage = (props: Props) => {
             </h2>
             <div className="flex flex-row gap-10 flex-wrap">
                 {
-                dummyData.map((data) => {
+                dummyStoryFiles.map((story) => {
                     return <div className="flex flex-col items-center pt-5 h-[200px] w-[200px] bg-black rounded-lg shirnk-0">
-                        <div className="bg-white h-[150px] w-[150px]"></div>
+                        <div className="bg-white h-[150px] w-[150px]">
+                            <DisplayStory filename={story.filesource} />
+                        </div>
+                        
                     </div>
                 })
             }
